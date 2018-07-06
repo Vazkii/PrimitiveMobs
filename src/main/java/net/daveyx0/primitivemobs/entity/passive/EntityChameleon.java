eafb9c3bbe8467f2f9690b42a4bfa15d1dfa44ab
ALongStringOfNumbers <sjensen793@gmail.com>
2018-07-05 20:58:31 -0700

    Remove import changes

--- src/main/java/net/daveyx0/primitivemobs/entity/passive/EntityChameleon.java
+++ src/main/java/net/daveyx0/primitivemobs/entity/passive/EntityChameleon.java
@@ -32,8 +32,7 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
-import net.minecraft.block.Block;
-import net.minecraftforge.common.property.IExtendedBlockState;
+
 
 public class EntityChameleon extends EntityTameable
 {
